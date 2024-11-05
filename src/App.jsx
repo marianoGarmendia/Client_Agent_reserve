10e3fb8a3f6e25d53e1bf67769a1e7f778760805
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-11-05 12:00:50 -0300

    logs cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 function App() {
-  console.log(import.meta.url);
-
   return (
     <main className="h-dvh flex flex-col w-full items-center justify-center">
       <Routes>
